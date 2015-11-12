a3efff26d930b21eb1277c4ca766b7ccca651de1
kamonchat <jj_jusjoy@hotmail.com>
2015-11-12 14:59:24 +0700

    spinner successful

--- app/src/main/java/npru/sangarun/kamonchat/mumaroi/MainActivity.java
+++ app/src/main/java/npru/sangarun/kamonchat/mumaroi/MainActivity.java
@@ -2,10 +2,13 @@ package npru.sangarun.kamonchat.mumaroi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
     //Explicit
     private ManageTABLE objManageTABLE;
+    private Spinner typeSpinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -13,9 +16,21 @@ public class MainActivity extends AppCompatActivity {
 
 
         //REquest Database
-        objManageTABLE = new ManageTABLE(this   );
+        objManageTABLE = new ManageTABLE(this);
+
+        //Create Spinner
+        createSpinner();
     }   // Main Method
 
+    private void createSpinner() {
+
+
+        typeSpinner = (Spinner) findViewById(R.id.spinner);
+        String[] strType = getResources().getStringArray(R.array.type);
+        ArrayAdapter<String> typeAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, strType);
+        typeSpinner.setAdapter(typeAdapter);
+
+    }
 
 
 }   // Main Class
