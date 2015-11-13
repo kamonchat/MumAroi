338dbfd4948cc4955bc73d1bbfdab3c29129440b
kamonchat <jj_jusjoy@hotmail.com>
2015-11-13 14:55:04 +0700

    Detail

--- app/src/main/java/npru/sangarun/kamonchat/mumaroi/RestaurList.java
+++ app/src/main/java/npru/sangarun/kamonchat/mumaroi/RestaurList.java
@@ -1,14 +1,19 @@
 package npru.sangarun.kamonchat.mumaroi;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telecom.Call;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 public class RestaurList extends AppCompatActivity {
     //Explicit
     private int indexAnInt;
-    private String[] nameStrings, typeStrings, iconStrings;
+    private String[] nameStrings, typeStrings, iconStrings,
+            detailStrings, potentialStrings, latStrings, lngStrings, remarkStrings;
     private int[] iconInts;
 
     @Override
@@ -30,10 +35,34 @@ public class RestaurList extends AppCompatActivity {
         ListView objListView = (ListView) findViewById(R.id.listView);
         objListView.setAdapter(objMyAdapter);
 
+        objListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Intent objIntent = new Intent(RestaurList.this, DetailActivity.class);
+                objIntent.putExtra("Name", nameStrings[position]);
+                objIntent.putExtra("Type", typeStrings[position]);
+                objIntent.putExtra("Image", iconInts[position]);
+                objIntent.putExtra("Detail", detailStrings[position]);
+                objIntent.putExtra("Potential", potentialStrings[position]);
+                objIntent.putExtra("Lat", latStrings[position]);
+                objIntent.putExtra("Lng", lngStrings[position]);
+                objIntent.putExtra("Remark", remarkStrings[position]);
+
+
+
+                startActivity(objIntent);
+
+            }
+        });
+
+
+
 
     }   // createListView
 
     private void receiveData() {
+
         indexAnInt = getIntent().getIntExtra("index", 0);
         Log.d("aoi", "index ==> " + Integer.toString(indexAnInt));
 
@@ -45,6 +74,11 @@ public class RestaurList extends AppCompatActivity {
                 nameStrings = objManageTABLE.readAllData(2);
                 typeStrings = objManageTABLE.readAllData(1);
                 iconStrings = objManageTABLE.readAllData(3);
+                detailStrings = objManageTABLE.readAllData(4);
+                potentialStrings = objManageTABLE.readAllData(5);
+                latStrings = objManageTABLE.readAllData(6);
+                lngStrings = objManageTABLE.readAllData(7);
+                remarkStrings = objManageTABLE.readAllData(8);
 
                 int intTimes = 0;
                 iconInts = new int[iconStrings.length];
