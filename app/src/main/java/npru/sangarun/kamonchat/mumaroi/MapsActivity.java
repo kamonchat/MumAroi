b3b090e4dcbac5f126d2bfc28f97fecef1304750
kamonchat <jj_jusjoy@hotmail.com>
2015-12-15 13:14:24 +0700

    center map

---
+++ app/src/main/java/npru/sangarun/kamonchat/mumaroi/MapsActivity.java
@@ -0,0 +1,64 @@
+package npru.sangarun.kamonchat.mumaroi;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private LatLng centerLatLng;
+    private String centerString, latString, lngString;
+    private double latADouble,lngADouble;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+
+        //Receive Value From Intent
+        receiveValue();
+
+        //Create LatLng for center Map
+        CreateLatLng();
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    } // Main Method
+
+    private void CreateLatLng() {
+
+        latADouble = Double.parseDouble(latString);
+        lngADouble = Double.parseDouble(lngString);
+        centerLatLng = new LatLng(latADouble,lngADouble);
+
+    }
+
+    private void receiveValue() {
+
+        centerString = getIntent().getStringExtra("Name");
+        latString = getIntent().getStringExtra("Lat");
+        lngString = getIntent().getStringExtra("Lng");
+
+
+    }   // receiveValue
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        //Create Center Map
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(centerLatLng, 16));
+
+    }   // onMapReady
+}   // Main Class
